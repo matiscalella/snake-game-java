8da47a88367ae046fc1d11f69ae9ae425ae7cbce
Matias Scalella <matias.scalella@gmail.com>
2025-11-19 20:57:12 -0300

    Agrego metodo y condiciones de fin de juego

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList; // Para manejar la lista de segmentos de la serpiente
 import java.util.Random; // Para generar posiciones aleatorias de la comida
 import javax.swing.*;
@@ -22,6 +23,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
 
 // ------ Cabeza de la serpiente
     Tile snakeHead; 
+    ArrayList<Tile> snakeBody = new ArrayList<>(); // Lista para los segmentos del cuerpo de la serpiente
 
 // ------ Comida
     Tile food;
@@ -31,6 +33,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
     Timer gameLoop;
     int velocityX;
     int velocityY;
+    boolean gameOver = false;
     
     public SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -41,6 +44,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
         setFocusable(true);
 
         snakeHead = new Tile(5, 5);
+        snakeBody = new ArrayList<Tile>(); // Inicializar la lista del cuerpo de la serpiente
 
         food = new Tile(10, 10);
         random = new Random();
@@ -68,8 +72,6 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
             g.drawLine(i * tileSize, 0, i * tileSize, boardHeight);
             g.drawLine(0, i * tileSize, boardWidth, i * tileSize);
         }
-
-
         // Dibujar la comida
         g.setColor(Color.RED);
         g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
@@ -78,8 +80,11 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
         g.setColor(Color.GREEN);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
 
-
-        
+        // Dibujar el cuerpo de la serpiente
+        for (int i = 0; i < snakeBody.size(); i++) {
+            Tile snakePart = snakeBody.get(i);
+            g.fillRect(snakePart.x * tileSize, snakePart.y * tileSize, tileSize, tileSize);
+        }        
     }
 // ------- Funcion para colocar la comida en una posicion aleatoria
     public void placeFood() {
@@ -87,16 +92,55 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
         food.y = random.nextInt(boardHeight / tileSize); // Numero aleatorio entre 0 y el numero de losetas en el alto (24)
     }
 
+// ------ Funcion para detectar colisiones entre dos losetas
+    public boolean collision(Tile tile1, Tile tile2) {
+        return tile1.x == tile2.x && tile1.y == tile2.y;
+    }
+
     public void move() {
-        // Mover la cabeza de la serpiente
+        if (collision(snakeHead, food)) {
+            snakeBody.add(new Tile(food.x, food.y)); // Agregar un nuevo segmento al cuerpo de la serpiente
+            placeFood(); // Colocar nueva comida
+        }
+
+        // Mover el cuerpo de la serpiente para seguir a la cabeza
+        for (int i = snakeBody.size() - 1; i >= 0; i--) {
+            Tile snakePart = snakeBody.get(i);
+            if (i == 0) {
+                snakePart.x = snakeHead.x;
+                snakePart.y = snakeHead.y;
+            } else {
+                Tile prevSnakePart = snakeBody.get(i - 1);
+                snakePart.x = prevSnakePart.x;
+                snakePart.y = prevSnakePart.y;
+            }
+        }
+        // Actualizar la posicion de la cabeza de la serpiente
         snakeHead.x += velocityX;
         snakeHead.y += velocityY;
+
+        // Verificar colision con el cuerpo de la serpiente
+        for (int i = 0; i < snakeBody.size(); i++) {
+            Tile snakePart = snakeBody.get(i);
+            if (collision(snakeHead, snakePart)) {
+                gameOver = true;
+            }
+        }
+        // Verificar colision con los bordes del tablero
+        if (snakeHead.x*tileSize < 0 || snakeHead.x*tileSize > boardWidth ||
+            snakeHead.y*tileSize < 0 || snakeHead.y*tileSize > boardHeight) {
+            gameOver = true;
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move(); // Mover la serpiente
         repaint(); // Llama a paintComponent para redibujar el juego
+        if (gameOver) {
+            gameLoop.stop();
+        }
+
     }
 
 // ------ Metodo para manejar las teclas presionadas
